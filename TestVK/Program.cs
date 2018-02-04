09137dc7eca402c5213e5ee35284d2077c93cab1
AndreySmirennikov <redpoker@ya.ru>
2018-02-04 19:33:59 +0300

    Add files via upload

---
+++ TestVK/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        
+        
+        public static void Main(string[] args)
+        {
+            //я выбрал передачу параметров запроса через aргументы командной строки
+            //Потому, что мне кажется это самый удобный вариант
+            //Однако  код ниже легко преобразовать под любой другой  ввод парамтров  для вызова API
+            String request="https://api.vk.com/method/"+args[1]+"?";
+                                                                    
+            
+            for (int i = 2; i <args.Length; i++)
+            {
+                if(i==2)request=request+args[i];
+                else request=request+"&"+args[i];
+            }
+            
+            
+            ProcessRepositories(request,args[0]).Wait();
+            
+            
+        }
+
+        
+        private static async Task ProcessRepositories(string request,string name)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            var stringTask = client.GetStringAsync(request);
+            var msg = await stringTask;
+            Console.Write(msg);
+
+            FileStream file1;
+            String fullPath = Path.GetFullPath("reports");
+            if(File.Exists(fullPath+"\\"+name+".txt"))file1 = new FileStream(fullPath+"\\"+name+".txt", FileMode.Append);
+            else file1 = new FileStream(fullPath+"\\"+name+".txt", FileMode.OpenOrCreate); 
+            StreamWriter writer = new StreamWriter(file1);  
+            writer.WriteLine("=========================={0}====================================\n",DateTime.Now.ToString("dd MMMM yyyy | HH:mm:ss"));
+            writer.WriteLine("Request:{0}",request);
+            writer.WriteLine("Response:{0}\n",msg);
+            writer.WriteLine("==============================================================\n");    
+            writer.Close(); 
+
+
+        }
+    }
+}
---
+++ TestVK/run.bat
@@ -0,0 +1,3 @@
+dotnet run report auth.signup first_name=Ivan last_name=Ivanov birthday=02.11.1990 client_id=6358046 client_secret=UQpAWIRdigIn5P1U0ve3 phone=89819878159 password=123123Aa test_mode=1 voice=0 sex=2 v=5.71
+dotnet run report auth.signup first_name=Ivan last_name=Ivanov birthday=02.11.1990 client_id=6358046 client_secret=UQpAWIRdigIn5P1U0ve3 phone= password=123123Aa test_mode=1 voice=0 sex=2 v=5.71
+pause;
