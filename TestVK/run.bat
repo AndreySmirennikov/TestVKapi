0e62b4293c62129b933aadeaebcbb007e5913e5d
AndreySmirennikov <redpoker@ya.ru>
2018-02-04 19:33:43 +0300

    Delete run.bat

--- TestVK/run.bat
+++
@@ -1,3 +0,0 @@
-dotnet run auth.signup first_name=Ivan last_name=Ivanov birthday=02.11.1990 client_id=6358046 client_secret=UQpAWIRdigIn5P1U0ve3 phone=89819878156 password=123123Aa test_mode=1 voice=0 sex=2 v=5.71
-dotnet run auth.signup first_name=Ivan last_name=Ivanov birthday=02.11.1990 client_id=6358046 client_secret=UQpAWIRdigIn5P1U0ve3 phone= password=123123Aa test_mode=1 voice=0 sex=2 v=5.71
-pause;
